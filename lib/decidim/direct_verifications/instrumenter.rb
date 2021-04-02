b65dc20a6e59a29cda63a106831af5146b3c1ede
Pau Perez <saulopefa@gmail.com>
2021-04-02 19:32:57 +0200

    Move methods within their class

--- lib/decidim/direct_verifications/instrumenter.rb
+++ lib/decidim/direct_verifications/instrumenter.rb
@@ -9,6 +9,14 @@ module Decidim
         @processed = { registered: [], authorized: [], revoked: [] }
       end
 
+      def add_processed(type, email)
+        @processed[type] << email
+      end
+
+      def add_error(type, email)
+        @errors[type] << email
+      end
+
       def processed_count(key)
         processed[key].size
       end
@@ -24,14 +32,6 @@ module Decidim
         else
           add_error event, email
         end
-      end
-
-      def add_error(type, email)
-        @errors[type] << email unless @errors[type].include? email
-      end
-
-      def add_processed(type, email)
-        @processed[type] << email unless @processed[type].include? email
       end
 
       private
